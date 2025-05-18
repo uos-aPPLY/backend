e8147e8b7e9e9c0ee9c435405c51ad3553960b35
nyungovo <nyungovo@gmail.com>
2025-05-18 14:26:18 +0900

    feat(global): add HealthCheckController for ALB health check

---
+++ src/main/java/com/apply/diarypic/global/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.apply.diarypic.global.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
